14df51d5c2bb8493e453924cc79dc1ebf1f235ec
Prakhar Pratap <prakharpratap2001@gmail.com>
2023-09-23 17:39:50 +0530

    easy

---
+++ Easy/118. Pascal'sTriangle.py
@@ -0,0 +1,26 @@
+"""
+118. Pascal's Triangle
+Given an integer numRows, return the first numRows of Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+"""
+
+
+class Solution:
+    def generate(self, numRows):
+        if numRows <= 0:
+            return []
+
+        triangle = [[1]]  # initializing the triangle with the first row
+
+        while len(triangle) < numRows:
+            prev_row = triangle[-1] # get the previous row 
+            new_row = [1]
+
+            for i in range(1, len(prev_row)):
+                new_element = prev_row[i-1] + prev_row[i]
+                new_row.append(new_element)
+
+            new_row.append(1) # the last element of each row is always 1
+            triangle.append(new_row)
+
+        return triangle 
---
+++ Medium/15. 3Sum.py
@@ -0,0 +1,35 @@
+"""
+15. 3Sum
+Given an integer array nums, return all the triplets [nums[i], nums[j], 
+nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+"""
+
+class Solution:
+    def threeSum(self, nums):
+        triplets = []
+        nums.sort() # sort the input array in non decreasing order
+
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue # skip duplicate value of nums[i]
+
+            left, right = i+1, len(nums)-1 # two pointer approach
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    triplets.append([nums[i], nums[left], nums[right]])
+                    # skip duplicate values of nums[left] and nums[right]
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return triplets
