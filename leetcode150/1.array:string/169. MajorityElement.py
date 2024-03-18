dd87e954a1475a9d561d29f258c4742505ec6729
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-03-19 00:14:31 +0530

    easy

---
+++ leetcode150/1.array:string/169. MajorityElement.py
@@ -0,0 +1,23 @@
+"""
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+"""
+
+
+def majorityElement(nums):
+    # boyer-moore voting algo
+    result, count = None, 0
+
+    for num in nums:
+        if count == 0:
+            result = num
+        if num == result:
+            count += 1
+        else:
+            count -= 1
+
+    return result
+
+
+print(majorityElement([1, 1, 2, 2, 2, 1, 1]))
--- leetcode150/1.array:string/26. remove_duplicates_from_sorted_array.py
+++ leetcode150/1.array:string/26. remove_duplicates_from_sorted_array.py
@@ -3,6 +3,7 @@ the duplicates in-place such that each unique element appears only once.
 The relative order of the elements should be kept the same. Then return 
 the number of unique element in nums."""
 
+
 def removeDuplicates(nums) -> int:
     if not nums:
         return 0
@@ -17,5 +18,6 @@ def removeDuplicates(nums) -> int:
         fast += 1
 
     return slow + 1
+
 
 print(removeDuplicates([1, 2, 2, 2, 3, 4, 4, 5]))
