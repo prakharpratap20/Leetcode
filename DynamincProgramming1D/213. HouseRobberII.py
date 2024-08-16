e2b978519c5e9824c372ac074afe7bbc0161924a
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-08-16 23:09:02 +0530

    comments

--- DynamincProgramming1D/213. HouseRobberII.py
+++ DynamincProgramming1D/213. HouseRobberII.py
@@ -1,10 +1,12 @@
 class Solution:
     def rob(self, nums):
+        # if there is only one house, rob it
         return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
 
     def helper(self, nums):
         rob1, rob2 = 0, 0
 
+        # [rob1, rob2, n, n+1, ...]
         for n in nums:
             newRob = max(rob1 + n, rob2)
             rob1 = rob2
--- DynamincProgramming1D/5. LongestPalindromicSubstring.py
+++ DynamincProgramming1D/5. LongestPalindromicSubstring.py
@@ -3,9 +3,11 @@ class Solution:
         res = ""
         resLen = 0
 
+        # for each character in the string
         for i in range(len(s)):
             # odd length
             left, right = i, i
+            # while left and right are within the bounds of the string and the characters at left and right are the same
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 if (right - left + 1) > resLen:
                     res = s[left: right + 1]
@@ -15,6 +17,7 @@ class Solution:
 
             # even length
             left, right = i, i + 1
+            # while left and right are within the bounds of the string and the characters at left and right are the same
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 if (right - left + 1) > resLen:
                     res = s[left: right + 1]
